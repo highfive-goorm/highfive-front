5f74709333777c33833effec4953d6c601955586
ksee1230 <ksee1230@gmail.com>
2025-05-08 09:59:50 +0900

    feat (HG-72): 장바구니 페이지 개발 및 테스트
    - .env.development 수정
        - 따옴표를 빼야함
        - 정해진 데이터를 반환하는 테스트를 진행 중인지 true false로 표기
    - 로그인 관련 기능 수정
        - lib/api.js를 api/auth.js, api/index.js로 분리
        - 이후 api 관련 파일은 해당 폴더에서 관리
        - auth.js에 로그인 테스트 용 데이터 설정
        - AuthContext.jsx에서 token, account 대신 accessToken, refreshToken
    - components/PrivateRoute.jsx
        - 로그인 상태일 때만 접근 가능할 페이지가 여럿인 듯 하여, wrapper 생성
    - JWT token 복호화 라이브러리 추가
    - 장바구니 페이지 개발
        - api/cart.js: 장바구니 관련 API 함수
            - 사용자 장바구니 조회
            - 장바구니 상품 수량 조절
            - 장바구니 상품 삭제
        - cart.js 테스트
            - 메모리 상에서 동작할 스텁용 데이터 저장소 생성
            - 해당 데이터를 통해 화면 렌더링
        - hooks/useCart.js: 장바구니 상태, 로직 훅
        - components/CartItem.jsx: 단일 상품 줄
            - 상품 이미지, 제목, 가격, 수량조절, 삭제
            - 임의 설정. 추후 변경
        - components/CartSummary.jsx: 그 외 구성
            - 합계, 배송비, 총액, 결제 버튼
        - pages/Cart.jsx: 장바구니 페이지

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = (props) => {
         <nav className="header__menu">
           <ul>
             <li className="menu-item"><Link to="/">마이페이지</Link></li>
-            <li className="menu-item"><Link to="/">장바구니</Link></li>
+            <li className="menu-item"><Link to="/cart">장바구니</Link></li>
             <li className="menu-item"><Link to="/">공지사항</Link></li>
           </ul>
         </nav>
