db9e3ef5269e60a305f40a05ec099a0040ef94d5
turnaroundwoo <seowooo3117@gmail.com>
2025-05-12 14:02:20 +0900

    feat (HG-76): 검색 페이지에 필터 및 상품리스트 추가

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
-import Image from '../components/Image'
+import ProductList from '../components/ProductList';
 
 const Search = () => {
     const [searchParams] = useSearchParams();
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState(''); 
+    // searchTerm : React state에 저장된 값으로 useState로 관리되는 상태 값(비동기) 
+    // => setState 후에 바로 사용 시, 값이 최신이 아닐 수 있으므로 초기 요청에는 query 사용 권장
 
     useEffect(() => {
-        const query = searchParams.get('q');
+        const query = searchParams.get('q'); // 즉시 추출된 값
         setSearchTerm(query || '');
-    }, [searchParams]);
+        console.log(`👉🏻 검색된 키워드 : ${query}`); // searchTerm 대신 query
+    }, [searchParams]); 
+
+    // [백엔드 요청 관련 주석 처리]
+    //useEffect(() => {
+    //    const query = searchParams.get('q') || '';
+    //    setSearchTerm(query);
+    //  
+    //    // 백엔드 요청에 바로 사용하려면 query 사용
+    //    fetch(`/api/search?q=${query}`);
+    //  }, [searchParams]);
 
     return (
         <div className="search-page">
@@ -19,8 +31,7 @@ const Search = () => {
                 <h1 className="search-title">검색된 키워드 : {searchTerm}</h1>
             </div>
             <div className="search-container">
-
-                <Image element="section nexon" title="상품 리스트" />
+                <ProductList />
             </div>
         </div>
     );
